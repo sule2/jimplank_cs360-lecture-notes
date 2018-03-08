5ce270f9bac3e5c8e59fedde1a8f9885e59dec67
Jim Plank <plank@cs.utk.edu>
2018-03-08 10:50:04 -0500

    Working.

--- Malloc1/fb2a.c
+++ Malloc1/fb2a.c
@@ -1,4 +1,4 @@
-/* fb2.c
+/* fb2a.c
    James S. Plank
    CS360 -- Systems Programming
    Malloc lecture #1
@@ -10,7 +10,7 @@
 
 int main()
 {
-  int *i1, *i2;
+  int *i1, *i2, *i3;
 
   printf("sbrk(0) before malloc(4): 0x%x\n", sbrk(0));
 
@@ -19,7 +19,11 @@ int main()
 
   i2 = (int *) malloc(4);
   printf("sbrk(0) after `i2 = (int *) malloc(4)': 0x%x\n", sbrk(0));
+  printf("i1 = 0x%x, i2 = 0x%x, sbrk(0)-i2 = %d\n", (unsigned int) i1, (unsigned int) i2, 
+             (unsigned int) ((char *) sbrk(0)- (char *) i2));
 
-  printf("i1 = 0x%x, i2 = 0x%x\n", (unsigned int) i1, (unsigned int) i2);
+  i3 = (int *) malloc(1024*132);
+  printf("sbrk(0) after `i3 = (int *) malloc(8164)': 0x%x\n", sbrk(0));
+  printf("i3 = 0x%x\n", (unsigned int) i3);
   return 0;
 }
