1c5ff3b3c08fdc10bc631a0128900888091683f9
Jim Plank <plank@cs.utk.edu>
2018-03-08 10:51:53 -0500

    Writing.

---
+++ Malloc1/fb3.c
@@ -0,0 +1,19 @@
+/* fb3.c
+   James S. Plank
+   CS360 -- Systems Programming
+   Malloc lecture #1
+   October, 1996 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int j, *buf;
+
+  for (j = 0; j < 10; j++) {
+    buf = (int *) malloc(4);
+    printf("malloc(4) returned 0x%x\n", (unsigned int) buf);
+  }
+  return 0;
+}
