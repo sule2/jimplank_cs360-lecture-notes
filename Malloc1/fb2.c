5c67c8c65b4a661ade00c0c032b2fdfc266f36b5
Jim Plank <plank@cs.utk.edu>
2018-03-08 10:33:19 -0500

    Malloc1.

---
+++ Malloc1/fb2.c
@@ -0,0 +1,24 @@
+/* fb2.c
+   James S. Plank
+   CS360 -- Systems Programming
+   Malloc lecture #1
+   October, 1996 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+int main()
+{
+  int *i1, *i2;
+
+  printf("sbrk(0) before malloc(4): 0x%x\n", sbrk(0));
+
+  i1 = (int *) malloc(4);
+  printf("sbrk(0) after `i1 = (int *) malloc(4)': 0x%x\n", sbrk(0));
+
+  i2 = (int *) malloc(4);
+  printf("sbrk(0) after `i2 = (int *) malloc(4)': 0x%x\n", sbrk(0));
+
+  printf("i1 = 0x%x, i2 = 0x%x\n", (unsigned int) i1, (unsigned int) i2);
+}
