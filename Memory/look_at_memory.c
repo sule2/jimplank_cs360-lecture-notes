484fd6a3851539799fd1e409bb5ac66041908bd5
Jim Plank <plank@cs.utk.edu>
2018-03-16 15:40:30 -0400

    Writing.

--- Memory/look_at_memory.c
+++ Memory/look_at_memory.c
@@ -4,24 +4,45 @@
 #include <unistd.h>
 
 typedef UI (unsigned long int) 
+
+/* These are special variables that
+   denote the end of the memory segments
+   for the code ("text"), initialized
+   global variables ("data") and
+   uninitialized global variables ("bss").
+   */
+
 extern end;
 extern etext;
 extern edata;
+
+/* Here are four global variables.  A
+   and B belong to the "data", and X and Y
+   belong to the "bss".  */
 
 int A = 4;
 int X;
 int B = 6;
 int Y;
 
+/* Proc_a and main should reside in the
+   "text" segment. */
+
 void proc_a()
 {
 }
 
+/* And of course i and buf will belong
+   on the stack.  I will call malloc()
+   to set buf equal to an address in 
+   the heap. */
+
 int main(int argc, char **argv)
 {
-  int i, j;
-  int *ii;
-  char buf[200];
+  int i;
+  char *buf;
+
+  buf = (char *) malloc(200);
 
   printf("Page size: %d\n", getpagesize());
   printf("\n");
@@ -42,16 +63,18 @@ int main(int argc, char **argv)
   printf("&Y: 0x%lx\n", (UI) &Y);
   printf("\n");
 
-  printf("Heap Addresses:\n");
-  ii = (int *) malloc(sizeof(int));
-  printf("ii: 0x%lx\n", (UI) ii);
+  printf("Heap Address:\n");
+  printf("buf: 0x%lx\n", (UI) buf);
   printf("\n");
 
   printf("Stack Addresses:\n");
   printf("&i:    0x%lx\n", (UI) &i);
-  printf("&j:    0x%lx\n", (UI) &j);
+  printf("&buf:  0x%lx\n", (UI) &buf);
   printf("&argc: 0x%lx\n", (UI) &argc);
   printf("\n");
+
+  /* Finally, print the addressses as recorded
+     in the directory "/proc". */
 
   sprintf(buf, "cat /proc/%d/maps", getpid());
   system(buf);
