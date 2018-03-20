d34e07b8f6ec340b9e78314b5007eda8dea29cbf
Jim Plank <plank@cs.utk.edu>
2018-03-20 11:43:02 -0400

    Pointers.

--- Memory/break_the_heap.c
+++ Memory/break_the_heap.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc)
+int main()
 {
   int i;
   char *c;
 
   for (i = 0; i < 1000000000; i++) {
     c = (char *) malloc(1024*1024);
-    printf("%9d 0x%08x\n", i, c);
+    printf("%9d 0x%08lx\n", i, (unsigned long) c);
     if (c == NULL) exit(0);
   }
   return 0;
