ac60dda0dbe14904241df020d2279edc054a741e
Jim Plank <plank@cs.utk.edu>
2019-01-24 13:53:48 -0500

    JRB.

--- JRB/golf.c
+++ JRB/golf.c
@@ -5,6 +5,11 @@
 #include "fields.h"
 #include "dllist.h"
 
+/* We have one of these for every golfer.  Each golfer will end up being on two
+   red-black trees.  The first is one keyed on the golfer's name, so that we can
+   find the golfer when we read in the score files.  The second is keyed first
+   on the golfer's tournaments-played, and then average score. */
+
 typedef struct {
   char *name;
   int ntourn;
@@ -12,10 +17,16 @@ typedef struct {
   Dllist scores;
 } Golfer;
 
+/* For each golfer, we are going to track his score in each tournament that he
+   played in.  This data structure keeps that information. */
+
 typedef struct {
   char *tname;
   int score;
 } Score;
+
+/* This is how we compare golfers in the second of the red-black trees described above.
+   First we compare their number of tournaments, and then their average score. */
 
 int golfercomp(Jval j1, Jval j2)
 {
@@ -31,12 +42,14 @@ int golfercomp(Jval j1, Jval j2)
   return 0;
 }
 
-
 int main(int argc, char **argv)
 {
+  JRB golfers;          /* This is the red-black tree sorted by golfer name. */
+  JRB sorted_golfers;   /* This is the red-black tree sorted with the above comparison fxn. */
+
+  JRB rnode;            /* These are all temporary variables. */
   Golfer *g;
   Score *s;
-  JRB golfers, rnode, sorted_golfers;
   int i, fn;
   int tmp;
   IS is;
@@ -48,11 +61,16 @@ int main(int argc, char **argv)
     exit(1);
   }
 
+  /* In this large block of code, we are reading in all of the golfers from the 
+     files, and storing them in the tree (golfers) sorted by name. */
+
   golfers = make_jrb();
 
   for (fn = 1; fn < argc; fn++) {
     is = new_inputstruct(argv[fn]);
     if (is == NULL) { perror(argv[fn]); exit(1); }
+
+    /* Error check */
 
     while(get_line(is) >= 0) {
       if (is->NF < 4 || strcmp(is->fields[is->NF-2], "F") != 0 ||
@@ -62,12 +80,16 @@ int main(int argc, char **argv)
         exit(1);
       }
       
+      /* Create the golfer's name inefficiently (I'm being lazy and calling strcat) */
+
       strcpy(name, is->fields[0]);
       for (i = 1; i < is->NF-3; i++) {
         strcat(name, " ");
         strcat(name, is->fields[i]);
       }
       
+      /* Find the golfer, and if he's not there, create him */
+
       rnode = jrb_find_str(golfers, name);
       if (rnode == NULL) {
         g = (Golfer *) malloc(sizeof(Golfer));
@@ -80,6 +102,9 @@ int main(int argc, char **argv)
         g = (Golfer *) rnode->val.v;
       }
 
+      /* Create the score struct for the golfer/tournament and 
+         add it to the golfer's score list. */
+
       s = (Score *) malloc(sizeof(Score));
       s->tname = argv[fn];
       s->score = atoi(is->fields[is->NF-1]);
@@ -90,7 +115,7 @@ int main(int argc, char **argv)
     jettison_inputstruct(is);
   }
 
-  /* Now, build a tree sorted on number of tournaments and lowest score.
+  /* Now, build the second tree sorted on number of tournaments and lowest score.
      We'll do this by using a general purpose rb-tree where the comparison
      function compares golfers using number of tournaments and lowest score */
 
@@ -112,5 +137,6 @@ int main(int argc, char **argv)
       printf("  %3d : %s\n", s->score, s->tname);
     }
   }
+  return 0;
 }
 
--- JRB/pga-convert.sh
+++ JRB/pga-convert.sh
@@ -1,3 +1,5 @@
+# Cut and paste the golfer lines from the "PAST RESULTS" link on pgatour.com
+
 if [ $# -ne 1 ]; then
   echo 'usage: sh pga-convert.sh 72-hole-par' >&2
   exit 1
